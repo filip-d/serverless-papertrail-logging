debe72d32a98121685df7c2544b34c0b05bf7e07
filip-d <filip@example.com>
2018-08-01 14:37:04 +0200

    removed lodash fixed bugs

--- src/logger.handler.js
+++ src/logger.handler.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
 const papertrail = require('winston-papertrail').Papertrail;
 const winston = require('winston');
 const zlib = require('zlib');
@@ -48,34 +47,45 @@ exports.handler = (event, context, callback) => {
     humanReadableUnhandledException: false,
   //  logFormat: formatLog,
   });
-  const loggers = {
-    "error" : logger.error,
-    "warning": logger.warning,
-    "info" : logger.info,
-    "debug" : logger.debug
-  }
+  logger.setLevels(winston.config.syslog.levels);
+    const payload = new Buffer(event.awslogs.data, 'base64');
+    zlib.gunzip(payload, (err, result) => {
+        if (err) {
+            return callback(err);
+        }
 
-  const payload = new Buffer(event.awslogs.data, 'base64');
-  zlib.gunzip(payload, (err, result) => {
-    if (err) {
-      return callback(err);
-    }
+        const logData = JSON.parse(result.toString('utf8'));
+        if (logData.messageType === 'CONTROL_MESSAGE') {
+            return callback();
+        }
 
-    const logData = JSON.parse(result.toString('utf8'));
-    if (logData.messageType === 'CONTROL_MESSAGE') {
-      return callback();
-    }
+        logData.logEvents.forEach((line) => {
+            if (line.message && !line.message.startsWith('START RequestId') && !line.message.startsWith('END RequestId')
+                && !line.message.startsWith('REPORT RequestId')) {
+                const logEntries = line.message.split('\t')
+                if (logEntries.length >= 4) {
+                    papertrail.program = logEntries[4];
+                    const message = logEntries.slice(4).join('\t');
+                    switch (logEntries[2].toLowerCase()) {
+                        case "error":
+                            logger.error(message);
+                            break;
+                        case "warning":
+                            logger.warning(message);
+                            break;
+                        case "info":
+                            logger.warning(message);
+                            break;
+                        default:
+                            logger.debug(message);
+                     }
+               } else {
+                    logger.debug(line.message)
+                }
+            }
+        });
 
-    logData.logEvents.forEach((line) => {
-      const logEntries = line.split('\t')
-      if (logEntries.length >= 4 && logEntries[2] !== "RequestId:") {
-        papertrail.program = logEntries[4];
-        const message = logEntries.slice(4).join('\t');
-        loggers[logEntries[3].toLowerCase()](message);
-      }   
+        logger.close();
+        return callback();
     });
-
-    logger.close();
-    return callback();
-  });
 };
